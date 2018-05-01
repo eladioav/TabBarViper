c246573d563f74157c23251fbb94effe9060bd0f
eladioav <eladioav@hotmail.com>
2018-05-01 10:41:28 -0700

    Initial Commit

---
+++ TabBarViper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarViper
+//
+//  Created by Eladio Alvarez Valle on 5/1/18.
+//  Copyright © 2018 Eladio Alvarez Valle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
